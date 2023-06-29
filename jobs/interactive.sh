68c81f8dbe5627b913c6813a9abe8fd60a200b78
Benjamin MacLellan <bmaclell@uwaterloo.ca>
2023-06-29 13:49:37 -0400

    Test flax NN on graham

--- jobs/interactive.sh
+++ jobs/interactive.sh
@@ -7,4 +7,6 @@ salloc  --time=03:00:00 --mem=3000M --gres=gpu:1
 
 source ~/bash-profiles/queso
 source ~/venv/queso_venv/bin/activate
+module load cuda/11.4
+module load cudnn/8.2.0
 module load python/3.9 scipy-stack
