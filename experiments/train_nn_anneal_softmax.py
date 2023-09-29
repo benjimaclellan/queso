80216b6111748a0c8edf257e9b0999d2b7fb88d1
Benjamin MacLellan <bmaclell@uwaterloo.ca>
2023-09-29 10:21:25 -0400

    Add softmax annealing test

--- experiments/train_nn_anneal_softmax.py
+++ experiments/train_nn_anneal_softmax.py
@@ -15,7 +15,8 @@ import jax.numpy as jnp
 import optax
 from flax import linen as nn
 from flax.training import train_state, orbax_utils
-from orbax.checkpoint import PyTreeCheckpointer, Checkpointer, CheckpointManager, CheckpointManagerOptions, PyTreeCheckpointHandler
+from orbax.checkpoint import PyTreeCheckpointer, Checkpointer, CheckpointManager, CheckpointManagerOptions, \
+    PyTreeCheckpointHandler
 
 from queso.estimators.flax.dnn import BayesianDNNEstimator
 from queso.sensors.tc.sensor import sample_int2bin
@@ -23,20 +24,19 @@ from queso.io import IO
 from queso.configs import Configuration
 from queso.utils import get_machine_info
 
-
 # %%
-io = IO(folder="test_multi_shot_training", include_date=True)
+io = IO(folder="test_softmax_annealing", include_date=True)
 config = Configuration()
 key = jax.random.PRNGKey(1234)
 plot = False
 progress = True
 
-#%%
-n_grid = config.n_grid
+# %%
+# n_grid = config.n_grid
 n_grid = 200
 nn_dims = config.nn_dims + [n_grid]
 lr = config.lr_nn
-l2_regularization = config.l2_regularization
+l2_regularization = 0.0  # config.l2_regularization
 n_epochs = config.n_epochs
 batch_size = config.batch_size
 from_checkpoint = config.from_checkpoint
@@ -52,17 +52,17 @@ probs = jnp.array(hf.get("probs"))
 phis = jnp.array(hf.get("phis"))
 hf.close()
 
-#%%
+# %%
 n = shots.shape[2]
 n_shots = shots.shape[1]
 n_phis = shots.shape[0]
 
-#%%
+# %%
 assert n_shots % batch_size == 0
 n_batches = n_shots // batch_size
 n_steps = n_epochs * n_batches
 
-#%%
+# %%
 dphi = phis[1] - phis[0]
 phi_range = (jnp.min(phis), jnp.max(phis))
 
@@ -83,58 +83,14 @@ model = BayesianDNNEstimator(nn_dims)
 x = shots
 y = labels
 
-#%%
+# %%
 x_init = x[1:10, 1:10, :]
 print(model.tabulate(jax.random.PRNGKey(0), x_init))
 
 
 # %%
-def l2_loss(w, alpha):
-    return alpha * (w ** 2).mean()
-
-
-@jax.jit
-def train_step(state, batch):
-    x_batch, y_batch = batch
-
-    def loss_fn(params):
-        logits = state.apply_fn({'params': params}, x_batch)
-
-        log_probs = jax.nn.log_softmax(logits_multi, axis=-1)
-
-        # standard cross-entropy
-        loss = -jnp.sum(y_batch[:, None, :] * log_probs, axis=-1).mean(axis=(0, 1))
-
-        loss += sum(
-            l2_loss(w, alpha=l2_regularization)
-            for w in jax.tree_leaves(params)
-        )
-        return loss
-
-    loss_val_grad_fn = jax.value_and_grad(loss_fn)
-    loss, grads = loss_val_grad_fn(state.params)
-
-    state = state.apply_gradients(grads=grads)
-    return state, loss
-
-
-#%%
-def shuffle_shots(shots, key=None):
-    if key is None:
-        key = jax.random.PRNGKey(time.time_ns())
-    shots_multi = copy.deepcopy(shots)
-    shots_multi = jax.random.shuffle(key, shots_multi, axis=1)
-    splits = jnp.split(
-        shots_multi[:, :jnp.floor(shots_multi.shape[1]/n_splits).astype('int') * n_splits, :],
-        n_splits,
-        axis=1
-    )
-    shots_multi = jnp.stack(splits, axis=0)
-    return shots_multi
-
-
 # %%
-def create_train_state(model, init_key, x, learning_rate):
+def create_train_state(model, init_key, x, lr):
     if from_checkpoint:
         ckpt_dir = io.path.joinpath("ckpts")
         ckptr = Checkpointer(PyTreeCheckpointHandler())  # A stateless object, can be created on the fly.
@@ -149,80 +105,132 @@ def create_train_state(model, init_key, x, learning_rate):
     # schedule = optax.constant_schedule(lr)
     schedule = optax.polynomial_schedule(
         init_value=lr,
-        end_value=lr**2,
+        end_value=lr ** 2,
         power=1,
-        transition_steps=n_steps//4,
-        transition_begin=3 * n_steps//2,
+        transition_steps=n_steps // 4,
+        transition_begin=3 * n_steps // 2,
     )
+
+
     tx = optax.adam(learning_rate=schedule)
     # tx = optax.adamw(learning_rate=learning_rate, weight_decay=1e-5)
 
-    state = train_state.TrainState.create(apply_fn=model.apply, params=params, tx=tx)
+    state = train_state.TrainState.create(
+        apply_fn=model.apply,
+        params=params,
+        tx=tx,
+    )
     return state
 
-init_key = jax.random.PRNGKey(time.time_ns())
-state = create_train_state(model, init_key, x_init, learning_rate=lr)
-# del init_key
 
 #%%
+def l2_loss(w, alpha):
+    return alpha * (w ** 2).mean()
+
+
+@jax.jit
+def train_step(state, batch, epoch):
+    x_batch, y_batch = batch
+
+    def loss_fn(params):
+        logits = state.apply_fn({'params': params}, x_batch)
+
+        temp = annealing_schedule(epoch)
+        log_probs = jax.nn.log_softmax(logits / temp, axis=-1)
+
+        # CE
+        loss = -jnp.sum(y_batch[:, None, :] * log_probs, axis=-1).mean(axis=(0, 1))
+
+        # loss += sum(
+        #     l2_loss(w, alpha=l2_regularization)
+        #     for w in jax.tree_leaves(params)
+        # )
+
+        return loss
+
+    loss_val_grad_fn = jax.value_and_grad(loss_fn)
+    loss, grads = loss_val_grad_fn(state.params)
+
+    state = state.apply_gradients(grads=grads)
+    return state, loss
+
+
+# %%
+def shuffle_shots(shots, key=None):
+    if key is None:
+        key = jax.random.PRNGKey(time.time_ns())
+    permutation = jax.random.permutation(key, shots.shape[1])  # shuffle only along the second axis
+    shots = jnp.take(shots, permutation, axis=1)
+    return shots
+
+
+# %%
 lr = 0.1
-n_epochs = 100
-n_splits = 2
+n_epochs = 500
+
+init_key = jax.random.PRNGKey(time.time_ns())
+state = create_train_state(model, init_key, x_init, lr=lr)
+
+annealing_schedule = optax.polynomial_schedule(
+    init_value=1.0,
+    end_value=0.01,
+    power=1,
+    transition_steps=n_epochs // 2,
+    transition_begin=n_epochs // 2,
+)
 
 keys = jax.random.split(key, (n_epochs))
 metrics = []
 pbar = tqdm.tqdm(total=n_epochs, disable=(not progress), mininterval=0.333)
-for i in range(n_epochs):  # number of epochs
-    shots_multi = shuffle_split_stack_shots(shots, n_splits=n_splits)
+for epoch in range(n_epochs):  # number of epochs
+    shots = shuffle_shots(shots)
+    batch = (shots, y)
 
-    batch = (shots_multi, y)
-
-    state, loss = train_step(state, batch)
+    state, loss = train_step(state, batch, epoch)
     if progress:
         pbar.update()
-        pbar.set_description(f"Epoch {i} | Loss: {loss:.10f}", refresh=False)
-    metrics.append(dict(step=i, loss=loss))
+        pbar.set_description(f"Epoch {epoch} | Loss: {loss:.10f}", refresh=False)
+    metrics.append(dict(step=epoch, loss=loss))
 
 pbar.close()
 metrics = pd.DataFrame(metrics)
 
-#%%
+# %%
 fig, ax = plt.subplots()
 ax.plot(metrics.step, metrics.loss)
 io.save_figure(fig, filename='nn_loss.png')
 
-#%%
-logits_multi = state.apply_fn({'params': state.params}, shots_multi)
-# log_probs_multi = jax.nn.log_softmax(logits_multi, axis=-1)
+# %%
+logits = state.apply_fn({'params': state.params}, shots)
+probs = jax.nn.softmax(logits, axis=-1)
 # probs = jax.nn.softmax(logits_multi, axis=-1).squeeze()
-prod_logits_multi = prod(logits_multi)
+# prod_logits_multi = prod(logits_multi)
 
 fig, ax = plt.subplots()
-ax.plot(prod_logits_multi[0, 0, :])
+ax.plot(probs[0, 0, :])
 io.save_figure(fig, filename="test_probs.png")
 
-#%%
+# %%
 x_batch = x[:, 0:batch_size, :]
 y_batch = y
 batch = (x_batch, y_batch)
 
 state, loss = train_step(state, batch)
 
-#%%
+# %%
 hf = h5py.File(io.path.joinpath("nn.h5"), "w")
 hf.create_dataset("grid", data=grid)
 hf.close()
 
-#%% compute posterior
+# %% compute posterior
 # approx likelihood from relative frequencies
 freqs = counts / counts.sum(axis=1, keepdims=True)
 likelihood = freqs
 
-bit_strings = sample_int2bin(jnp.arange(2**n), n)
+bit_strings = sample_int2bin(jnp.arange(2 ** n), n)
 pred = model.apply({'params': state.params}, bit_strings)
 pred = jax.nn.softmax(pred, axis=-1)
 posterior = pred
-
 
 # %% save to disk
 metadata = dict(nn_dims=nn_dims, lr=lr, time=time.time() - t0)
@@ -243,9 +251,9 @@ restored = ckptr.restore(ckpt_dir, item=None)
 
 print(f"Finished training the estimator.")
 
-#%%
+# %%
 if plot:
-    #%% plot prior
+    # %% plot prior
     # fig, ax = plt.subplots()
     # ax.stem(prior)
     # fig.show()
@@ -274,7 +282,7 @@ if plot:
                 ls='',
                 marker=next(markers),
                 color=colors[i],
-                label=r"Pr($\phi_j | "+"b_{"+str(i)+"}$)")
+                label=r"Pr($\phi_j | " + "b_{" + str(i) + "}$)")
 
         xdata = jnp.linspace(phi_range[0], phi_range[1], counts.shape[0], endpoint=False)
         # if not jnp.all(jnp.isnan(probs)).item():
@@ -287,5 +295,8 @@ if plot:
 
     plt.show()
 
+# from queso.benchmark_estimator import benchmark_estimator
 
-
+# benchmark_estimator(
+#     io, config, key,
+# )
